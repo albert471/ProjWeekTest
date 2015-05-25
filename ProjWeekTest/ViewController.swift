558c5b02bf4da957ac67c105ab6218249d33350c
Albert Checcone <Albert-Alessia@alberts-air.attlocal.net>
2015-05-25 15:46:15 -0700

    Initial Commit

---
+++ ProjWeekTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjWeekTest
+//
+//  Created by Albert Checcone on 5/25/15.
+//  Copyright (c) 2015 Albert Checcone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
