2945a559ba4621fef291fc399674009b7e5794c7
albert471 <acheccone19@students.polytechnic.org>
2015-05-25 15:57:30 -0700

    stuff
    added basic stuff

---
+++ ProjWeekTest WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  ProjWeekTest WatchKit Extension
+//
+//  Created by Albert Checcone on 5/25/15.
+//  Copyright (c) 2015 Albert Checcone. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
